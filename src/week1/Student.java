73a3beb1e261c8320e70decb1008c8dc4def9698
HoangDTung <43265415+HoangDTung@users.noreply.github.com>
2018-10-04 10:49:58 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -2,37 +2,67 @@ package week1;
 
 public class Student {
 
-    // TODO: khai báo các thuộc tính cho Student
+    //khai báo các thuộc tính cho Student
+    private String Name;
+    private String ID;
+    private String Group;
+    private String Email;
+    //khai báo các phương thức getter, setter cho Student
 
-    // TODO: khai báo các phương thức getter, setter cho Student
-
-    /**
-     * Constructor 1
-     */
-    Student() {
-        // TODO:
+    public String getName() {
+        return Name;
     }
 
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
-    Student(String n, String sid, String em) {
-        // TODO:
+    public void setName(String name) {
+        Name = name;
     }
 
-    /**
-     * Constructor 3
-     * @param s
-     */
-    Student(Student s) {
-        // TODO:
+    public String getID() {
+        return ID;
     }
 
-    String getInfo() {
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getGroup() {
+        return Group;
+    }
+
+    public void setGroup(String group) {
+        Group = group;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+    public Student() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        Name = "Student";
+        ID = "000";
+        Group = "INT22041";
+        Email = "uet@vnu.edu.vn";
+    }
+    public Student(String n, String sid, String em) {
+        // TODO:
+        Name = n;
+        ID = sid;
+        Email = em;
+        Group = "INT22041";
+    }
+    public Student(Student s) {
+        // TODO:
+        Name = s.Name;
+        ID = s.ID;
+        Group = s.Group;
+        Email = s.Email;
+    }
+
+    public String getInfo() {
+        return "Student{" +"Name='" + Name + '\'' +", ID='" + ID + '\'' +", Group='" + Group + '\'' +", Email='" + Email + '\'' +'}';
     }
 }
