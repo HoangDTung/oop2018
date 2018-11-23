2552c1265ce12d0f0684000103e9082da373329a
HoangDTung <43265415+HoangDTung@users.noreply.github.com>
2018-11-23 11:13:57 +0700

    up

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,30 @@
 package week11;
 
 public class Task1 {
+    public <T extends Comparable<T>> void sort(T[] a){
+        int n=a.length ;
+        for(int i=0;i<n-1;i++){
+            for(int j=i;j<n;j++){
+                if(a[i].compareTo(a[j])>0){
+                    T b=a[i];
+                    a[i]=a[j];
+                    a[j]=b;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+"  ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = { 1, -2, 3, -4, 5,0 };
+        Double[] doubleArray = { 1.2, 2.3, -3.4, 4.5,-1.2 ,3.7,10.5,999.9};
+        String [] stringArray={"b","Hoang","Duy","Tung","k62","uet"};
+        Task1 t1=new Task1();
+        t1.sort(intArray);
+        t1.sort(doubleArray);
+        t1.sort(stringArray);
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,36 @@
 package week11;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Task2 {
+    public <T extends Comparable<T>> T max(ArrayList<T> input){
+        T m=input.get(0);
+        for(int i=1;i<input.size();i++){
+            if(m.compareTo(input.get(i))<0){
+                m=input.get(i);
+            }
+        }
+        return m;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> intArray=new ArrayList<>();
+        ArrayList<String> stringArray=new ArrayList<>();
+        for(int i=0;i<10;i++){
+            Random rd=new Random();
+            intArray.add(rd.nextInt(1000));
+        }
+        for(int i=0;i<10;i++){
+            System.out.println(intArray.get(i)+" ");
+        }
+        Task2 t2=new Task2();
+        System.out.println("max: "+t2.max(intArray));
+        stringArray.add("Hoang");
+        stringArray.add("Duy");
+        stringArray.add("Tung");
+        stringArray.add("oop");
+        stringArray.add("k62");
+        System.out.println(t2.max(stringArray));
+    }
 }
